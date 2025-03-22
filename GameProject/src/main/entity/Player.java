06deaeb27ac05d79441337fe3a0cc508b0a98b84
LyesChikh <chikhlyes55@gmail.com>
2025-03-21 22:39:00 -0400

    Add files via upload

---
+++ GameProject/src/main/entity/Player.java
@@ -0,0 +1,33 @@
+
+public class Player {
+    private String name;
+    private int level;
+    private int health;
+    private int mana;
+
+
+    public Player(String name, int level, int health, int mana) {
+        this.name = name;
+        this.level = level;
+        this.health = health;
+        this.mana = mana;   
+    }
+
+    public String getName() {return name;}
+
+    public int getLevel() {return level;}
+
+    public int getHealth() {return health;}
+
+    public int getMana() {return mana;}
+
+    public void setName(String name) {this.name = name;}
+
+    public void setLevel(int level) {this.level = level;}
+
+    public void setHealth(int health) {this.health = health;}
+
+    public void setMana(int mana) {this.mana = mana;}
+
+
+}
